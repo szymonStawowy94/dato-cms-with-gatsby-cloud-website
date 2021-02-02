eb09cfa304acb7efaf38f61db42ffba198bb4db5
Szymon <s.stawowy@pleodigital.com>
2021-02-02 15:02:47 +0100

    map module test

---
+++ src/components/mapModule/SimpleMap.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+	height: 450px;
+`;
+
+const SingleMap = ({ lat, lng }) => {
+	const center = { lat, lng };
+	const googleMapsApiKey = process?.env?.GOOGLE_MAPS_API_KEY || '';
+	console.log(process);
+	return (
+		<StyledWrapper>
+			<h1>{googleMapsApiKey}</h1>
+			<h1>{process.env.GOOGLE_MAPS_API_KEY}</h1>
+			<GoogleMapReact
+				bootstrapURLKeys={{ key: process.env.GOOGLE_MAPS_API_KEY }}
+				defaultCenter={center}
+				defaultZoom={17}
+				center={center}
+			>
+			</GoogleMapReact>
+		</StyledWrapper>
+	);
+};
+
+export default SingleMap;
