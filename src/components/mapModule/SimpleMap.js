eb88e645b1429793c06d8d8949ae4ea3ba6a0524
Szymon <s.stawowy@pleodigital.com>
2021-02-02 15:25:57 +0100

    env changed

--- src/components/mapModule/SimpleMap.js
+++ src/components/mapModule/SimpleMap.js
@@ -2,20 +2,22 @@ import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import styled from 'styled-components';
 
+
 const StyledWrapper = styled.div`
 	height: 450px;
 `;
 
 const SingleMap = ({ lat, lng }) => {
 	const center = { lat, lng };
-	const googleMapsApiKey = process?.env?.GOOGLE_MAPS_API_KEY || '';
-	console.log(process);
+	const googleMapsApiKey = process.env.GATSBY_GOOGLE_MAPS_API_KEY || '';
+	console.log(process.env.GATSBY_GOOGLE_MAPS_API_KEY);
+	console.log(googleMapsApiKey)
 	return (
 		<StyledWrapper>
 			<h1>{googleMapsApiKey}</h1>
-			<h1>{process.env.GOOGLE_MAPS_API_KEY}</h1>
+			<h1>{process.env.GATSBY_GOOGLE_MAPS_API_KEY}</h1>
 			<GoogleMapReact
-				bootstrapURLKeys={{ key: process.env.GOOGLE_MAPS_API_KEY }}
+				bootstrapURLKeys={{ key: googleMapsApiKey}}
 				defaultCenter={center}
 				defaultZoom={17}
 				center={center}
