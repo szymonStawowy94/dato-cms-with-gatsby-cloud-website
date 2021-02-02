72cf2b60a262b07b2c62651abe968080635d3c84
Szymon <s.stawowy@pleodigital.com>
2021-02-02 15:30:33 +0100

    console.logs removed

--- src/components/mapModule/SimpleMap.js
+++ src/components/mapModule/SimpleMap.js
@@ -10,12 +10,8 @@ const StyledWrapper = styled.div`
 const SingleMap = ({ lat, lng }) => {
 	const center = { lat, lng };
 	const googleMapsApiKey = process.env.GATSBY_GOOGLE_MAPS_API_KEY || '';
-	console.log(process.env.GATSBY_GOOGLE_MAPS_API_KEY);
-	console.log(googleMapsApiKey)
 	return (
 		<StyledWrapper>
-			<h1>{googleMapsApiKey}</h1>
-			<h1>{process.env.GATSBY_GOOGLE_MAPS_API_KEY}</h1>
 			<GoogleMapReact
 				bootstrapURLKeys={{ key: googleMapsApiKey}}
 				defaultCenter={center}
